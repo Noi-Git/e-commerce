5ebe91247de9425de022aef5dbbaff8c4071491e
Noi-Git <noi.patsin@gmail.com>
2023-06-17 19:11:03 -0400

    refactor home page and app page

--- src/App.js
+++ src/App.js
@@ -1,36 +1,7 @@
-import '../src/components/category-item/category-item-styles.scss'
-import Directory from './components/directory/directory.component'
+import HomePage from './routes/home/HomePage'
 
-function App() {
-  const categories = [
-    {
-      id: 1,
-      title: 'hats',
-      imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-    },
-    {
-      id: 2,
-      title: 'jackets',
-      imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-    },
-    {
-      id: 3,
-      title: 'sneakers',
-      imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-    },
-    {
-      id: 4,
-      title: 'womens',
-      imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-    },
-    {
-      id: 5,
-      title: 'mens',
-      imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-    },
-  ]
-
-  return <Directory categories={categories} />
+const App = () => {
+  return <HomePage />
 }
 
 export default App
