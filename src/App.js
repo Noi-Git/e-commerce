cf2cfcdb495cb57739a9c909116513caa7673508
Noi-Git <noi.patsin@gmail.com>
2023-06-17 00:33:57 -0400

    setup basic category elements

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 function App() {
+  const categories = [
+    { id: 1, title: 'Hats' },
+    { id: 1, title: 'Jackets' },
+    { id: 1, title: 'Sneckers' },
+    { id: 1, title: 'Womens' },
+    { id: 1, title: 'Mans' },
+  ]
+
   return (
-    <div>
-      <h1>Hi There!</h1>
+    <div className='categories-container'>
+      {categories.map(({ title }) => (
+        <div className='category-container'>
+          {/* <img /> */}
+          <div className='category-body-container'>
+            <h2>{title}</h2>
+            <p>Shop Now</p>
+          </div>
+        </div>
+      ))}
     </div>
   )
 }
