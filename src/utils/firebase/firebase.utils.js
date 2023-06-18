db75d87f63013421747f152bca98495fe36aac0f
Noi-Git <noi.patsin@gmail.com>
2023-06-17 21:41:41 -0400

    setup firebase from your firebase doc homepage

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBkUGEeMeAFSOqjh28KOSnCIJh1zoFd8rw',
+  authDomain: 'noi-e-commerce-db.firebaseapp.com',
+  projectId: 'noi-e-commerce-db',
+  storageBucket: 'noi-e-commerce-db.appspot.com',
+  messagingSenderId: '650095256826',
+  appId: '1:650095256826:web:aa6f6ebfe962c16fab1543',
+}
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig)
