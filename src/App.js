22e56f9ae86c98ae16a9afa098feb2e87490b0fc
Noi-Git <noi.patsin@gmail.com>
2023-06-17 20:05:27 -0400

    refactor navigation bar to its own component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,6 @@
-import { Routes, Route, Outlet } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import HomePage from './routes/home/HomePage'
-
-const Navigation = () => {
-  return (
-    <div>
-      <div>
-        <h1>I am the navigation bar</h1>
-      </div>
-      <Outlet />
-    </div>
-  )
-}
+import Navigation from './routes/navigation/Navigation'
 
 const Shop = () => {
   return <h1>I am the shop page</h1>
