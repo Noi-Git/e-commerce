97d4b7695fbb634abe70cfd077021b6d948565d9
Noi-Git <noi.patsin@gmail.com>
2023-06-17 22:31:08 -0400

    check if user snapshot exist

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -27,4 +27,6 @@ export const createUserDocumentFromAuth = async (userAuth) => {
   const userDocRef = doc(db, 'users', userAuth.uid)
 
   console.log(userDocRef)
+  const userSnapshot = await getDoc(userDocRef)
+  console.log(userSnapshot.exists())
 }
