2bc3c1d5602ccc3bb402bca5546b5a89b05144dc
Noi-Git <noi.patsin@gmail.com>
2023-06-17 21:58:05 -0400

    setup firebase step 2

--- src/routes/sign-in/SignIn.jsx
+++ src/routes/sign-in/SignIn.jsx
@@ -1,7 +1,14 @@
+import { signInWithGooglePopup } from '../../utils/firebase/firebase.utils'
 const SignIn = () => {
+  const logGoogleUser = async () => {
+    const response = await signInWithGooglePopup()
+    console.log(response)
+  }
+
   return (
     <div>
       <h1>Sing In page</h1>
+      <button onClick={logGoogleUser}>Sing in wiht Google Popup</button>
     </div>
   )
 }
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,13 +1,20 @@
 import { initializeApp } from 'firebase/app'
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from 'firebase/auth'
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyBkUGEeMeAFSOqjh28KOSnCIJh1zoFd8rw',
-  authDomain: 'noi-e-commerce-db.firebaseapp.com',
-  projectId: 'noi-e-commerce-db',
-  storageBucket: 'noi-e-commerce-db.appspot.com',
-  messagingSenderId: '650095256826',
-  appId: '1:650095256826:web:aa6f6ebfe962c16fab1543',
-}
+const firebaseConfig = {}
 
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig)
+
+const provider = new GoogleAuthProvider()
+provider.setCustomParameters({
+  prompt: 'select_account',
+})
+
+export const auth = getAuth()
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider)
