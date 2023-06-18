1e66bb0b00be43e7086d12f2d7e8834cbfcdcedb
Noi-Git <noi.patsin@gmail.com>
2023-06-17 20:43:18 -0400

    install firebase, create sign in page and route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 import HomePage from './routes/home/HomePage'
 import Navigation from './routes/navigation/Navigation'
+import SignIn from './routes/sign-in/SignIn'
 
 const Shop = () => {
   return <h1>I am the shop page</h1>
@@ -12,6 +13,7 @@ const App = () => {
       <Route path='/' element={<Navigation />}>
         <Route index element={<HomePage />} />
         <Route path='shop' element={<Shop />} />
+        <Route path='signIn' element={<SignIn />} />
       </Route>
     </Routes>
   )
