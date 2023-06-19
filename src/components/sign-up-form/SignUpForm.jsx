25eefd4bedb6903c7b44559e71cddadd3f98158d
Noi-Git <noi.patsin@gmail.com>
2023-06-19 13:27:28 -0400

    create and assign onChange to each form field

--- src/components/sign-up-form/SignUpForm.jsx
+++ src/components/sign-up-form/SignUpForm.jsx
@@ -1,4 +1,18 @@
+import { useState } from 'react'
+
+const defaultFormFields = {
+  displayName: '',
+  email: '',
+  password: '',
+  confirmPassword: '',
+}
+
 const SignUpForm = () => {
+  const [formFields, setFormFields] = useState(defaultFormFields)
+  const { displayName, email, password, confirmPassword } = formFields
+
+  const handleChange = (event) => {}
+
   const onSubmitSignup = () => {}
 
   return (
@@ -10,13 +24,13 @@ const SignUpForm = () => {
         }}
       >
         <label>Display Name</label>
-        <input type='text' required />
+        <input type='text' required onChange={handleChange} />
         <label>Email</label>
-        <input type='email' required />
+        <input type='email' required onChange={handleChange} />
         <label>Password</label>
-        <input type='password' required />
+        <input type='password' required onChange={handleChange} />
         <label>Confirm Password</label>
-        <input type='password' required />
+        <input type='password' required onChange={handleChange} />
         <button type='submit'>Sign Up</button>
       </form>
     </div>
