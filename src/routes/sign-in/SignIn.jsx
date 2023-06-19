11610d36baf6d0e30e0ea0adfca969de215520c2
Noi-Git <noi.patsin@gmail.com>
2023-06-19 13:03:21 -0400

    final step of setup  sign in with google redirect

--- src/routes/sign-in/SignIn.jsx
+++ src/routes/sign-in/SignIn.jsx
@@ -1,10 +1,21 @@
+import { useEffect } from 'react'
+import { getRedirectResult } from 'firebase/auth'
 import {
+  auth,
   signInWithGooglePopup,
   singInWithGoogleRedirect,
   createUserDocumentFromAuth,
 } from '../../utils/firebase/firebase.utils'
 
 const SignIn = () => {
+  useEffect(async () => {
+    const response = await getRedirectResult(auth)
+    if (response) {
+      const userDocRef = await createUserDocumentFromAuth(response.user)
+    }
+    console.log(response)
+  }, [])
+
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup()
     console.log(user)
@@ -12,17 +23,17 @@ const SignIn = () => {
     const userDocRef = await createUserDocumentFromAuth(user)
   }
 
-  const logGoogleRedirectUser = async () => {
-    const { user } = await singInWithGoogleRedirect()
-    console.log(user)
+  // const logGoogleRedirectUser = async () => {
+  //   const { user } = await singInWithGoogleRedirect()
+  //   console.log(user)
 
-    // const userDocRef = await createUserDocumentFromAuth(user)
-  }
+  //   // const userDocRef = await createUserDocumentFromAuth(user)
+  // }
   return (
     <div>
       <h1>Sing In page</h1>
       <button onClick={logGoogleUser}>Sing in wiht Google Popup</button>
-      <button onClick={logGoogleRedirectUser}>
+      <button onClick={singInWithGoogleRedirect}>
         Sing in wiht Google Redirect
       </button>
     </div>
