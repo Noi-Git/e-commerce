d8079c0b60a8ac036da8fa76e796d85223631c78
Noi-Git <noi.patsin@gmail.com>
2023-06-19 13:05:52 -0400

    done learn googleRedirect signIn-not going to use

--- src/routes/sign-in/SignIn.jsx
+++ src/routes/sign-in/SignIn.jsx
@@ -1,21 +1,9 @@
-import { useEffect } from 'react'
-import { getRedirectResult } from 'firebase/auth'
 import {
-  auth,
   signInWithGooglePopup,
-  singInWithGoogleRedirect,
   createUserDocumentFromAuth,
 } from '../../utils/firebase/firebase.utils'
 
 const SignIn = () => {
-  useEffect(async () => {
-    const response = await getRedirectResult(auth)
-    if (response) {
-      const userDocRef = await createUserDocumentFromAuth(response.user)
-    }
-    console.log(response)
-  }, [])
-
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup()
     console.log(user)
@@ -23,19 +11,10 @@ const SignIn = () => {
     const userDocRef = await createUserDocumentFromAuth(user)
   }
 
-  // const logGoogleRedirectUser = async () => {
-  //   const { user } = await singInWithGoogleRedirect()
-  //   console.log(user)
-
-  //   // const userDocRef = await createUserDocumentFromAuth(user)
-  // }
   return (
     <div>
       <h1>Sing In page</h1>
       <button onClick={logGoogleUser}>Sing in wiht Google Popup</button>
-      <button onClick={singInWithGoogleRedirect}>
-        Sing in wiht Google Redirect
-      </button>
     </div>
   )
 }
