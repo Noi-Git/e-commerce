9274ad759351af2a8b38f60bbbf24669bc9d7275
Noi-Git <noi.patsin@gmail.com>
2023-06-19 22:32:18 -0400

    check if password match

--- src/components/sign-up-form/SignUpForm.jsx
+++ src/components/sign-up-form/SignUpForm.jsx
@@ -24,6 +24,10 @@ const SignUpForm = () => {
 
   const handleSubmit = async (event) => {
     event.preventDefault()
+
+    if (password !== confirmPassword) {
+      alert('Password does not match')
+    }
   }
 
   return (
