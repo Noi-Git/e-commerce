7df36c05a1fb68529824eda1f95ce4f7310446fd
Noi-Git <noi.patsin@gmail.com>
2023-06-23 17:52:07 -0400

    store data in UserProvider for its children to use

--- src/contexts/UserContext.jsx
+++ src/contexts/UserContext.jsx
@@ -1,4 +1,4 @@
-import { createContext } from 'react'
+import { createContext, useState } from 'react'
 
 /* === context ===
   - context is a kind of storage place
@@ -8,5 +8,11 @@ import { createContext } from 'react'
 export const UserContext = createContext({})
 
 export const UserProvider = ({ children }) => {
-  return <UserContext.Provider> {children} </UserContext.Provider>
+  const [currentUser, setCurrentUser] = useState(null)
+  const value = { currentUser, setCurrentUser }
+
+  //will receive value stored in currentUser and setCurrentUser
+  //this provider is allowing any of its children components to access the values inside of its useState
+  //<UseContext><App/></UseContext>
+  return <UserContext.Provider value={value}> {children} </UserContext.Provider>
 }
