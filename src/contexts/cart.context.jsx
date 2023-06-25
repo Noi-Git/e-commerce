bc970d96375bd6c8bd5fe91cdbb94399435692c3
Noi-Git <noi.patsin@gmail.com>
2023-06-25 15:44:47 -0400

    create addItemToCart function

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -21,7 +21,9 @@ export const CartProvider = ({ children }) => {
   const [isCartOpen, setIsCartOpen] = useState(false)
   const [cartItems, setCartItems] = useState([])
 
-  const addItemToCart = (productToAdd) => {}
+  const addItemToCart = (productToAdd) => {
+    setCartItems(addCartItem(cartItems, productToAdd))
+  }
 
   const value = { isCartOpen, setIsCartOpen }
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>
