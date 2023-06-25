74ee783e509c9563c94748a435b325b7dfd58998
Noi-Git <noi.patsin@gmail.com>
2023-06-25 17:03:30 -0400

    create checkout page and route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import HomePage from './routes/home/HomePage'
 import Navigation from './routes/navigation/Navigation'
 import Authentication from './routes/authentication/Authentication'
 import Shop from './routes/shop/Shop'
+import CheckOut from './routes/checkout/CheckOut'
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
         <Route index element={<HomePage />} />
         <Route path='shop' element={<Shop />} />
         <Route path='auth' element={<Authentication />} />
+        <Route path='checkout' element={<CheckOut />} />
       </Route>
     </Routes>
   )
---
+++ src/routes/checkout/CheckOut.jsx
@@ -0,0 +1,7 @@
+import './checkout.styles.scss'
+
+const CheckOut = () => {
+  return <div>CheckOut</div>
+}
+
+export default CheckOut
