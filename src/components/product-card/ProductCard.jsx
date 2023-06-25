cc9fb3cab4796e96b452d6350d3f03f8f0b785fc
Noi-Git <noi.patsin@gmail.com>
2023-06-24 23:00:37 -0400

    pass mock data into product card

--- src/components/product-card/ProductCard.jsx
+++ src/components/product-card/ProductCard.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import Button from '../button/Button'
 import './product-card.styles.scss'
 
-const ProductCard = () => {
+// const ProductCard = ({product}) => { //data in {product} comes from mock data
+const ProductCard = ({ product }) => {
+  const { name, price, imageUrl } = product
   return (
     <div className='product-card-container'>
-      <img />
+      <img src={imageUrl} alt={name} />
       <div className='footer'>
-        <span className='name'></span>
-        <span className='price'></span>
+        <span className='name'>{name}</span>
+        <span className='price'>{price}</span>
       </div>
       <Button buttonType='inverted'>Add to Card </Button>
     </div>
