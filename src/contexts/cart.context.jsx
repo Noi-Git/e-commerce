fb3d7a2aff5da659f358cdf94f829309977f347a
Noi-Git <noi.patsin@gmail.com>
2023-06-25 15:55:12 -0400

    add result from helper function to value

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -36,6 +36,6 @@ export const CartProvider = ({ children }) => {
     setCartItems(addCartItem(cartItems, productToAdd))
   }
 
-  const value = { isCartOpen, setIsCartOpen }
+  const value = { isCartOpen, setIsCartOpen, addItemToCart, cartItems }
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>
 }
