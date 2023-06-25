f3283408a01f60ddca009dd8640c05e980bbe6cd
Noi-Git <noi.patsin@gmail.com>
2023-06-25 15:39:56 -0400

    create utility function and add sudo code of logic

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -4,6 +4,12 @@ import { createContext, useState } from 'react'
 {id, name, price, imageUrl, quantity}
 */
 
+const addCartItem = (cartItems, productToAdd) => {
+  //find if cartItems contains productToAdd
+  //if found, increment quantity
+  //return new array with modifyied cartItems/new cart item
+}
+
 export const CartContext = createContext({
   isCartOpen: false,
   setIsCartOpen: () => {},
@@ -14,6 +20,8 @@ export const CartContext = createContext({
 export const CartProvider = ({ children }) => {
   const [isCartOpen, setIsCartOpen] = useState(false)
   const [cartItems, setCartItems] = useState([])
+
+  const addItemToCart = (productToAdd) => {}
 
   const value = { isCartOpen, setIsCartOpen }
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>
