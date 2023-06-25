6e5ed7d8890fc05bd234e11cb3fefd052bde4a28
Noi-Git <noi.patsin@gmail.com>
2023-06-25 17:51:27 -0400

    remove white space

--- src/routes/checkout/CheckOut.jsx
+++ src/routes/checkout/CheckOut.jsx
@@ -11,23 +11,18 @@ const CheckOut = () => {
     <div className='checkout-container'>
       <div className='checkout-header'>
         <div className='header-block'>
-          {' '}
           <span>Product</span>
         </div>
         <div className='header-block'>
-          {' '}
           <span>Description</span>
         </div>
         <div className='header-block'>
-          {' '}
           <span>Quantity</span>
         </div>
         <div className='header-block'>
-          {' '}
           <span>Price</span>
         </div>
         <div className='header-block'>
-          {' '}
           <span>Remove</span>
         </div>
       </div>
