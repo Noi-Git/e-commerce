6640c11448204c512786f590a4ab85e6f9ca3fe7
Noi-Git <noi.patsin@gmail.com>
2023-06-25 15:58:56 -0400

    map through value receive from cart context

--- src/components/cart-dropdown/CartDropdown.jsx
+++ src/components/cart-dropdown/CartDropdown.jsx
@@ -1,13 +1,16 @@
+import { useContext } from 'react'
+import { CartContext } from '../../contexts/cart.context'
 import Button from '../button/Button'
 import CartItem from '../cart-item/CartItem'
 import './cart-dropdown.styles.scss'
 
 const CartDropdown = () => {
+  const { cartItes } = useContext(CartContext)
   return (
     <div className='cart-dropdown-container'>
       <div className='cart-items'>
-        {[].map((item) => (
-          <CartItem cartItem={item} />
+        {cartItes.map((item) => (
+          <CartItem key={item.id} cartItem={item} />
         ))}
       </div>
       <Button>Checkout</Button>
