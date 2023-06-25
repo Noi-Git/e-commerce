e69bdddfaebb47872d00ea911bec498f97071b9c
Noi-Git <noi.patsin@gmail.com>
2023-06-25 17:50:36 -0400

    add header to checkout page

--- src/routes/checkout/CheckOut.jsx
+++ src/routes/checkout/CheckOut.jsx
@@ -8,7 +8,29 @@ const CheckOut = () => {
     useContext(CartContext)
 
   return (
-    <div>
+    <div className='checkout-container'>
+      <div className='checkout-header'>
+        <div className='header-block'>
+          {' '}
+          <span>Product</span>
+        </div>
+        <div className='header-block'>
+          {' '}
+          <span>Description</span>
+        </div>
+        <div className='header-block'>
+          {' '}
+          <span>Quantity</span>
+        </div>
+        <div className='header-block'>
+          {' '}
+          <span>Price</span>
+        </div>
+        <div className='header-block'>
+          {' '}
+          <span>Remove</span>
+        </div>
+      </div>
       <h1>Checkout page</h1>
       <div>
         {cartItems.map((cartItem) => {
