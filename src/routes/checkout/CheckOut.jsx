7ef90bceb6ee99c1aa23561f0d43926e2e6030b1
Noi-Git <noi.patsin@gmail.com>
2023-06-25 17:12:15 -0400

    import CartContext from cart.context

--- src/routes/checkout/CheckOut.jsx
+++ src/routes/checkout/CheckOut.jsx
@@ -1,6 +1,11 @@
+import { useContext } from 'react'
+import { CartContext } from '../../contexts/cart.context'
+
 import './checkout.styles.scss'
 
 const CheckOut = () => {
+  const { cartItems } = useContext(CartContext)
+
   return <div>CheckOut</div>
 }
 
