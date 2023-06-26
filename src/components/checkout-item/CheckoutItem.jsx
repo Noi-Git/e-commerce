091fc00cd226e203da59a92f5e9d4d40ae8fca5b
Noi-Git <noi.patsin@gmail.com>
2023-06-25 23:13:31 -0400

    add onClick to increase and decrease

--- src/components/checkout-item/CheckoutItem.jsx
+++ src/components/checkout-item/CheckoutItem.jsx
@@ -9,6 +9,8 @@ const CheckoutItem = ({ cartItem }) => {
     useContext(CartContext)
 
   const clearItemHandler = () => clearItemFromCart(cartItem)
+  const addItemHandler = () => addItemToCart(cartItem)
+  const removeItemHandler = () => removeItemFromCart(cartItem)
 
   return (
     <div className='checkout-item-container'>
@@ -17,11 +19,11 @@ const CheckoutItem = ({ cartItem }) => {
       </div>
       <span className='name'>{name}</span>
       <span className='quantity'>
-        <div className='arrow' onClick={addItemToCart}>
+        <div className='arrow' onClick={removeItemHandler}>
           &#10094;
         </div>
         <span className='value'>{quantity}</span>
-        <div className='arrow' onClick={removeItemFromCart}>
+        <div className='arrow' onClick={addItemHandler}>
           &#10095;
         </div>
       </span>
