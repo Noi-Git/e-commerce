58a6713bf82963dd27b11a3556d5d232b7a7997f
Noi-Git <noi.patsin@gmail.com>
2023-07-10 10:16:22 -0400

    add style for styled component to the component

--- src/components/cart-dropdown/CartDropdown.jsx
+++ src/components/cart-dropdown/CartDropdown.jsx
@@ -3,7 +3,11 @@ import { useNavigate } from 'react-router-dom'
 import { CartContext } from '../../contexts/cart.context'
 import Button from '../button/Button'
 import CartItem from '../cart-item/CartItem'
-import './cart-dropdown.styles.jsx'
+import {
+  CartDropdownContainer,
+  CartItems,
+  EmptyMessage,
+} from './cart-dropdown.styles.jsx'
 
 const CartDropdown = () => {
   const { cartItems } = useContext(CartContext)
@@ -14,16 +18,16 @@ const CartDropdown = () => {
   }
 
   return (
-    <div className='cart-dropdown-container'>
-      <div className='cart-items'>
+    <CartDropdownContainer>
+      <CartItems>
         {cartItems.length ? (
           cartItems.map((item) => <CartItem key={item.id} cartItem={item} />)
         ) : (
-          <span>Your cart is empty</span>
+          <EmptyMessage>Your cart is empty</EmptyMessage>
         )}
-      </div>
+      </CartItems>
       <Button onClick={goToCheckoutHandler}> Checkout</Button>
-    </div>
+    </CartDropdownContainer>
   )
 }
 
