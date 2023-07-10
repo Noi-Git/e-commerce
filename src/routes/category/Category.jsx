3bafb9096ba3c6b200bc6d43d375d81cf58d999e
Noi-Git <noi.patsin@gmail.com>
2023-07-09 22:45:03 -0400

    bring in categoriesMap from CategoriesContext

--- src/routes/category/Category.jsx
+++ src/routes/category/Category.jsx
@@ -1,8 +1,12 @@
+import { useContext } from 'react'
 import { useParams } from 'react-router-dom'
+import { CategoriesContext } from '../../contexts/categories.context'
+
 import './category.styles.scss'
 
 const Category = () => {
   const { category } = useParams()
+  const { categoriesMap } = useContext(CategoriesContext)
 }
 
 export default Category
