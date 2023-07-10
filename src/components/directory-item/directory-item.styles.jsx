ce6adc1eab9f2c39fd9909aabccc81c6419fbd5b
Noi-Git <noi.patsin@gmail.com>
2023-07-10 11:28:38 -0400

    converted DirectoryItemBody

--- src/components/directory-item/directory-item.styles.jsx
+++ src/components/directory-item/directory-item.styles.jsx
@@ -10,6 +10,30 @@ background-position: center;
 background-image: ${({imageUrl}) => `url(${imageUrl})`}
 `
 
+export const DirectoryItemBody = styled.div`
+height: 90px;
+padding: 0 25px;
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: center;
+border: 1px solid black;
+background-color: white;
+opacity: 0.7;
+position: absolute;
+
+h2 {
+  margin: 0 6px 0;
+  font-size: 22px;
+  color: #4a4a4a;
+}
+
+p {
+  font-weight: lighter;
+  font-size: 16px;
+}
+`
+
 export const DirectoryItemContainer = styled.div`
 
 `
@@ -54,27 +78,8 @@ export const DirectoryItemContainer = styled.div`
   }
 
   .directory-item-body {
-    height: 90px;
-    padding: 0 25px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    border: 1px solid black;
-    background-color: white;
-    opacity: 0.7;
-    position: absolute;
 
-    h2 {
-      // font-weight: bold;
-      margin: 0 6px 0;
-      font-size: 22px;
-      color: #4a4a4a;
-    }
 
-    p {
-      font-weight: lighter;
-      font-size: 16px;
-    }
+    
   }
 }
