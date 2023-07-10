3fbe321313a722a42b56d516c5ecd76a9787266b
Noi-Git <noi.patsin@gmail.com>
2023-07-09 22:04:56 -0400

    change form ( ) to use { } for multiple lines return

--- src/routes/shop/Shop.jsx
+++ src/routes/shop/Shop.jsx
@@ -1,18 +1,13 @@
-// import SHOP_DATA from '../../shop-data.json'
 import { Fragment, useContext } from 'react'
 import { CategoriesContext } from '../../contexts/categories.context'
-// import ProductCard from '../../components/product-card/ProductCard'
 import './shop.styles.scss'
 import CategoryPreview from '../../components/category-preview/CategoryPreview'
-
 
 const Shop = () => {
   const { categoriesMap } = useContext(CategoriesContext)
   return (
-    <div className="shop-container">
-      {Object.keys(categoriesMap).map((title) => (
-
-      ))}
+    <div className='shop-container'>
+      {Object.keys(categoriesMap).map((title) => {})}
     </div>
   )
 }
