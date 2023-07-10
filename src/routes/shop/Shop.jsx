dbb6eaff4840ffc394832b880ac43540194378ca
Noi-Git <noi.patsin@gmail.com>
2023-07-09 22:03:43 -0400

    use CategoryPreview instead of ProductCard

--- src/routes/shop/Shop.jsx
+++ src/routes/shop/Shop.jsx
@@ -1,24 +1,19 @@
 // import SHOP_DATA from '../../shop-data.json'
 import { Fragment, useContext } from 'react'
 import { CategoriesContext } from '../../contexts/categories.context'
-import ProductCard from '../../components/product-card/ProductCard'
+// import ProductCard from '../../components/product-card/ProductCard'
 import './shop.styles.scss'
+import CategoryPreview from '../../components/category-preview/CategoryPreview'
+
 
 const Shop = () => {
   const { categoriesMap } = useContext(CategoriesContext)
   return (
-    <Fragment>
+    <div className="shop-container">
       {Object.keys(categoriesMap).map((title) => (
-        <Fragment key={title}>
-          <h2>{title}</h2>
-          <div className='products-container'>
-            {categoriesMap[title].map((product) => (
-              <ProductCard key={product.id} product={product} />
-            ))}
-          </div>
-        </Fragment>
+
       ))}
-    </Fragment>
+    </div>
   )
 }
 
