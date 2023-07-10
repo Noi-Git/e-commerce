828cb7c1212d3617e400518506e16b1fcabbb474
Noi-Git <noi.patsin@gmail.com>
2023-07-10 11:38:32 -0400

    add upper case to styled component

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -12,7 +12,7 @@ const DirectoryItem = ({ category }) => {
       <BackgroundImage imageUrl={imageUrl}></BackgroundImage>
       <DirectoryItemBody>
         <Link to='/shop'>
-          <h2>{title.toUpperCase()}</h2>
+          <h2>{title}</h2>
           <p>SHOP NOW</p>
         </Link>
       </DirectoryItemBody>
