1a8fe04635c988dc1d89ad62f9de9c923aaf3539
Noi-Git <noi.patsin@gmail.com>
2023-07-09 23:54:02 -0400

    add link to items on the main page to navigate to shop page

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import './directory-item.styles.scss'
 
 const DirectoryItem = ({ category }) => {
@@ -9,8 +10,10 @@ const DirectoryItem = ({ category }) => {
         style={{ backgroundImage: `url(${imageUrl})` }}
       ></div>
       <div className='directory-item-body'>
-        <h2>{title.toUpperCase()}</h2>
-        <p>SHOP NOW</p>
+        <Link to='/shop'>
+          <h2>{title.toUpperCase()}</h2>
+          <p>SHOP NOW</p>
+        </Link>
       </div>
     </div>
   )
